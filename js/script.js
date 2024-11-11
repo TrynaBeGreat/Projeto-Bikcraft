b3a81cc7050859f0e74e1630905ac7b8e29e365f
Lucas Gabriel <lucasfefe36@gmail.com>
2024-11-11 00:24:43 -0300

    menu-ativo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("teste");
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLinks(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLinks);
+
+// Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  elemento.checked = true;
+  console.log(parametro);
+}
+
+parametros.forEach(ativarProduto);
